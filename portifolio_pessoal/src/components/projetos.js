3eb5a88cc147d83f54b68ee122a4b5c9aa57b2da
Nome Sobrenome <rafaelconquista2002@hotmail.com>
2023-12-23 00:17:59 -0300

    adiciona inicio do desenvolvimento do modal

--- portifolio_pessoal/src/components/projetos.js
+++ portifolio_pessoal/src/components/projetos.js
@@ -3,6 +3,7 @@ import perfil from '../img/perfil.jpg';
 import { MdKeyboardArrowDown } from "react-icons/md";
 import { MdKeyboardArrowUp } from "react-icons/md";
 import { useState } from 'react';
+import { Modal } from 'react-bootstrap';
 
 const projects = {
     'Projeto1': 'imagem', 'Projeto2': 'imagem', 'Projeto3': 'imagem', 'Projeto4': 'imagem', 'Projeto5': 'imagem', 'Projeto6': 'imagem', 'Projeto7': 'imagem',
@@ -10,6 +11,7 @@ const projects = {
 
 function Projetos() {
     const [showProjects, setShowProjects] = useState(true)
+    const [showModal, setShowModal] = useState(false);
     const handleProjects = () => {
         if (!showProjects) {
             setShowProjects(true)
@@ -17,6 +19,15 @@ function Projetos() {
             setShowProjects(false)
         }
     }
+
+    const handleModal = () => {
+        setShowModal(true);
+    };
+
+    const handleClose = () => {
+        setShowModal(false);
+    };
+
 
     return (
         <div id='projetos' className='projects_body'>
@@ -40,7 +51,7 @@ function Projetos() {
                         {
                             Object.keys(projects).map((key, icon) => {
                                 return (
-                                    <div className='projects_card'>
+                                    <div className='projects_card' onClick={handleModal}>
                                         <div className='imagem'>
                                             <img src={perfil} alt='vazio' className='slider_image' />
                                         </div>
@@ -52,7 +63,20 @@ function Projetos() {
 
                     </div>
                     : <div></div>}
-
+            <Modal show={showModal} onHide={handleClose}>
+                <Modal.Header closeButton>
+                    <Modal.Title>Meu Modal</Modal.Title>
+                </Modal.Header>
+                <Modal.Body>
+                    Aqui vai o conteúdo do seu modal.
+                </Modal.Body>
+                <Modal.Footer>
+                    <button variant="secondary" onClick={handleClose}>
+                        Fechar
+                    </button>
+                    {/* Adicione aqui outros botões ou ações no footer, se necessário */}
+                </Modal.Footer>
+            </Modal>
 
         </div>
     )
