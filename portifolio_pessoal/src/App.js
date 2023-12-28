5400500873cab5171172307923b69524104ea549
Nome Sobrenome <rafaelconquista2002@hotmail.com>
2023-12-28 18:03:55 -0300

    adiciona estilização modal

--- portifolio_pessoal/src/App.js
+++ portifolio_pessoal/src/App.js
@@ -11,7 +11,6 @@ function App() {
       <Navbar className="teste" />
       <Welcome />
       <Skills />
-      <Destaques />
       <Projetos />
       <Footer />
     </div>
--- portifolio_pessoal/src/components/projetos.js
+++ portifolio_pessoal/src/components/projetos.js
@@ -12,7 +12,8 @@ const projects = {
     'Projeto1': projectImage, 'Projeto2': projectImage, 'Projeto3': projectImage, 'Projeto4': projectImage, 'Projeto5': projectImage, 'Projeto6': projectImage, 'Projeto7': projectImage,
 }
 const projects_desc = {
-    'Projeto1': 'Descição completa do projeto, onde será adaptado de forma dinâmica', 'Projeto2': 'batata completa do projeto, onde será adaptado de forma dinâmica', 'Projeto3': 'Descição completa do projeto, onde será adaptado de forma dinâmica', 'Projeto4': 'Descição completa do projeto, onde será adaptado de forma dinâmica', 'Projeto5': 'Descição completa do projeto, onde será adaptado de forma dinâmica', 'Projeto6': 'Descição completa do projeto, onde será adaptado de forma dinâmica', 'Projeto7': 'Descição completa do projeto, onde será adaptado de forma dinâmica',
+    'Projeto1': "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla sit amet consequat magna, quis consequat mi. Nulla condimentum dictum nisi in vestibulum. Donec lacinia blandit vulputate. Donec nec porttitor metus. Aenean vel consectetur ante. Sed malesuada lacus sed mattis vestibulum. Sed neque lacus, pretium id dictum id, tristique id risus. Mauris id ultricies velit, id imperdiet nisi. Nulla facilisi. Quisque dictum nibh sed dignissim suscipit.Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla sit amet consequat magna, quis consequat mi. Nulla condimentum dictum nisi in vestibulum. Donec lacinia blandit vulputate. Donec nec porttitor metus. Aenean vel consectetur ante. Sed malesuada lacus sed mattis vestibulum. Sed neque lacus, pretium id dictum id, tristique id risus. Mauris id ultricies velit, id imperdiet nisi. Nulla facilisi. Quisque dictum nibh sed dignissim suscipit.Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla sit amet consequat magna, quis consequat mi. Nulla condimentum dictum nisi in vestibulum. Donec lacinia blandit vulputate. Donec nec porttitor metus. Aenean vel consectetur ante. Sed malesuada lacus sed mattis vestibulum. Sed neque lacus, pretium id dictum id, tristique id risus. Mauris id ultricies velit, id imperdiet nisi. Nulla facilisi. Quisque dictum nibh sed dignissim suscipit.Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla sit amet consequat magna, quis consequat mi. Nulla condimentum dictum nisi in vestibulum. Donec lacinia blandit vulputate. Donec nec porttitor metus. Aenean vel consectetur ante. Sed malesuada lacus sed mattis vestibulum. Sed neque lacus, pretium id dictum id, tristique id risus. Mauris id ultricies velit, id imperdiet nisi. Nulla facilisi. Quisque dictum nibh sed dignissim suscipit.Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla sit amet consequat magna, quis consequat mi. Nulla condimentum dictum nisi in vestibulum. Donec lacinia blandit vulputate. Donec nec porttitor metus. Aenean vel consectetur ante. Sed malesuada lacus sed mattis vestibulum. Sed neque lacus, pretium id dictum id, tristique id risus. Mauris id ultricies velit, id imperdiet nisi. Nulla facilisi. Quisque dictum nibh sed dignissim suscipit.Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla sit amet consequat magna, quis consequat mi. Nulla condimentum dictum nisi in vestibulum. Donec lacinia blandit vulputate. Donec nec porttitor metus. Aenean vel consectetur ante. Sed malesuada lacus sed mattis vestibulum. Sed neque lacus, pretium id dictum id, tristique id risus. Mauris id ultricies velit, id imperdiet nisi. Nulla facilisi. Quisque dictum nibh sed dignissim suscipit.",
+    'Projeto2': 'batata completa do projeto, onde será adaptado de forma dinâmica', 'Projeto3': 'Descição completa do projeto, onde será adaptado de forma dinâmica', 'Projeto4': 'Descição completa do projeto, onde será adaptado de forma dinâmica', 'Projeto5': 'Descição completa do projeto, onde será adaptado de forma dinâmica', 'Projeto6': 'Descição completa do projeto, onde será adaptado de forma dinâmica', 'Projeto7': 'Descição completa do projeto, onde será adaptado de forma dinâmica',
 }
 
 function Projetos() {
@@ -81,21 +82,24 @@ function Projetos() {
                 </Modal.Header>
                 <Modal.Body>
                     <Row className='modal_position'>
-                        <Col className='col-6'>
-                            <p>{projects_desc[project]}</p>
-                        </Col>
                         <Col className='d-flex'>
                             <img src={image} alt='vazio' className='projeto_imagem' />
+                        </Col>
+                        <Col className='col-6'>
+                            <p>{projects_desc[project]}</p>
                         </Col>
                     </Row>
                 </Modal.Body>
                 <Modal.Footer className='modal_footer'>
-                    <div className='botao' onClick={handleClose}>
-                        <span>Acessar</span>
+                    <div className='buttons'>
+                        <div className='botao' onClick={handleClose}>
+                            <span>Acessar</span>
+                        </div>
+                        <div className='botao' onClick={handleClose}>
+                            <span>Fechar</span>
+                        </div>
                     </div>
-                    <div className='botao' onClick={handleClose}>
-                        <span>Fechar</span>
-                    </div>
+                    
                 </Modal.Footer>
             </Modal>
 
